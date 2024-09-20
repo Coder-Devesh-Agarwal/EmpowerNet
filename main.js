8223cc926fb7a504147a0e8eb1ae0bf89798e2f2
Coder-Devesh-Agarwal <deveshagarwal1729@gmail.com>
2024-09-20 19:59:39 +0530

    added colors and some small changes

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const colors = require('colors/safe');
 
 // Create a new client instance
 const client = new Client();
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-  console.log('Client is ready!');
+// Start your client
+client.initialize();
+
+client.on('loading_screen', (percent, message) => {
+  console.log('LOADING SCREEN', percent, message);
 });
 
 // When the client received QR-Code
 client.on('qr', (qr) => {
+  // NOTE: This event will not be fired if a session is specified.
   qrcode.generate(qr, { small: true });
-  console.log('QR RECEIVED', qr);
+  console.log(colors.green('QR RECEIVED'));
 });
 
-// Start your client
-client.initialize();
+client.on('authenticated', () => {
+  console.log(colors.green('AUTHENTICATED'));
+});
+
+client.on('auth_failure', (msg) => {
+  // Fired if session restore was unsuccessful
+  console.error(colors.green('AUTHENTICATION FAILURE'), msg);
+});
+
+// When the client is ready, run this code (only once)
+client.on('ready', () => {
+  console.log(colors.green('EMPOWER-NET CLIENT IS READY'));
+});
